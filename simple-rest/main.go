54ff4a2339eca35b82d7889c6b44479f56a870a3
seongwon <seongwon@edu.hanbat.ac.kr>
2020-08-31 15:55:59 +0900

    golang simple rest api

--- simple-rest/main.go
+++ simple-rest/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
@@ -8,22 +9,42 @@ import (
 
 const PORT = ":10000"
 
+type Article struct {
+	Title string `json:"title"`
+	Desc string `json:"desc"`
+	Content string `json:"content"`
+}
+
+var Articles []Article
+
 func main() {
+
+	Articles = []Article{
+		{Title: "Hello", Desc: "seongwon", Content: "name"},
+		{Title: "World", Desc: "lucas", Content: "nickname"},
+	}
 
 	log.Println("server running at", PORT)
 	handleRequest()
 }
 
-func homePage(w http.ResponseWriter, r *http.Request){
+func homepage(w http.ResponseWriter, r *http.Request){
 
 	fmt.Fprintf(w, "Welcome to the seongwon")
-	fmt.Println("Endpoint Hit: homePage")
+	fmt.Println("Endpoint Hit: homepage")
 
+}
+
+func returnAllArticle(w http.ResponseWriter, r *http.Request)  {
+
+	fmt.Println("Endpoint Hit : return all article")
+	json.NewEncoder(w).Encode(Articles)
 }
 
 func handleRequest()  {
 
-	http.HandleFunc("/", homePage)
+	http.HandleFunc("/", homepage)
+	http.HandleFunc("/articles", returnAllArticle)
 	log.Fatal(http.ListenAndServe(PORT, nil))
 
 }
